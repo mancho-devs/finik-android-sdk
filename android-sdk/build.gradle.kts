5446e684f6860d02ae5d294e011e0f0cdcd12a85
Kubanych Asanov <55246776+kuba-asanov@users.noreply.github.com>
2025-07-22 15:24:17 +0600

    Released v1.5.0

--- android-sdk/build.gradle.kts
+++ android-sdk/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "kg.finik"
-version = "1.3.0"
+version = "1.5.0"
 
 android {
     namespace = "kg.finik.android.sdk"
@@ -108,7 +108,7 @@ afterEvaluate {
 }
 
 dependencies {
-    implementation("kg.finik:flutter_release:1.3.0")
+    implementation("kg.finik:flutter_release:1.5.0")
 //    implementation("kg.finik.flutter_module:flutter_release:1.2.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
